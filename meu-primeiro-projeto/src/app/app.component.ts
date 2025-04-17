2baa5b8f8499bf1b155f5d5dba20525e8f87e3ac
Ruan Portella <ruanmorales29@gmail.com>
2025-04-16 21:51:45 -0300

    Feat: Estudo inicial de angular com componentes

---
+++ meu-primeiro-projeto/src/app/app.component.ts
@@ -0,0 +1,16 @@
+import { CommonModule } from '@angular/common';
+import { Component } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+import { NewComponent } from './components/new-component/new-component.component';
+
+@Component({
+  selector: 'app-root',
+  imports: [CommonModule ,RouterOutlet, NewComponent],
+  template: `
+   <!-- <router-outlet /> -->
+
+    <h1>Meu primeiro projeto Angular</h1>
+    <app-new-component />
+  `
+})
+export class AppComponent {}
---
+++ meu-primeiro-projeto/src/app/components/new-component/new-component.component.ts
@@ -0,0 +1,12 @@
+import { CommonModule } from '@angular/common';
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-new-component',
+  imports: [CommonModule],
+  templateUrl: './new-component.component.html',
+  styleUrl: './new-component.component.scss'
+})
+export class NewComponent {
+
+}
